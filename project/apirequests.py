b87d810a6152a1af33fd6c6be3c72811e352c4bb
Ville Venetjoki <ville.venetjoki@tuni.fi>
2022-10-10 17:38:02 +0300

    Add template for apirequests

---
+++ project/apirequests.py
@@ -0,0 +1,25 @@
+"""
+This file fetches data from digitraffic and the fmi
+
+For data, a start and end time are required.
+
+For a forecast, the end time of the forecast is required.
+
+Requests are parsed into python dict containers and returned.
+"""
+
+
+def weather_data():
+    pass
+
+
+def weather_forecast():
+    pass
+
+
+def road_data():
+    pass
+
+
+def road_forecast():
+    pass
