63e2e38761c1d405dbfee51866f198aa8d6128de
Ilari <ilari.miettunen@hotmail.com>
2022-11-02 10:19:19 +0200

    Working version of graph with selections, not final

--- project/graph.py
+++ project/graph.py
@@ -19,36 +19,42 @@ dates = ['01,09,2022','02,09,2022','03,09,2022',
             '25,09,2022','26,09,2022','27,09,2022',
             '28,09,2022','29,09,2022','30,09,2022',]
 
-hours = ['08:00', '09:00', '10:00', '11:00', '12:00', '13:00'] 
-base = dt.datetime(2022, 9, 1, 8,0)
+hour_rain = {   
+    '08:00': 1.0, 
+    '09:00': 3.5, 
+    '10:00': 2.0, 
+    '11:00': 4.0, 
+    '12:00': 5.5, 
+    '13:00': 0.0   
+} 
+
+hour_temp = {
+    '08:00':8, 
+    '09:00':10, 
+    '10:00':12, 
+    '11:00':13, 
+    '12:00':17, 
+    '13:00':15
+} 
 
 daily_rain = []
 
 daily_temp = []
 
-sixh_temp = []
-
-sixh_rain = []
-
 def rand_rain():
     while len(daily_rain)<30:
        daily_rain.append(round(rand.uniform(0.0, 5.5),1))
 
-    while len(sixh_rain)<6:
-       sixh_rain.append(round(rand.uniform(0.5, 2.5),1))
 def rand_temp():
     while len(daily_temp)<30:
        daily_temp.append(rand.randint(-3, 18)) 
-    while len(sixh_temp)<6:
-       sixh_temp.append(rand.randint(7, 15))
-
 
 class MplCanvas(FigureCanvasQTAgg):
     def __init__(self, parent=None, width=5, height=4, dpi=100):
         fig = Figure(figsize=(width, height), dpi=dpi)
         self.ax1 = fig.add_subplot()
         self.ax2 = self.ax1.twinx()
-        #self.figure = plt.gcf()
+        #self.figure = fig
         
         super().__init__(fig)
 
@@ -57,6 +63,22 @@ class MatplotlibWidget(QMainWindow):
         super().__init__()
         vlayout = QVBoxLayout()
         hlayout = QHBoxLayout()
+        RBlayout = QGridLayout()
+        self.timespan = 'month'
+
+        monthRB = QRadioButton("month")
+        monthRB.setChecked(True)
+        #monthRB.country = "Australia"
+        monthRB.toggled.connect(self.onClickedMonth)
+        RBlayout.addWidget(monthRB, 0, 0)
+
+        sixhRB = QRadioButton("6h")
+        #sixhRB.country = "China"
+        sixhRB.toggled.connect(self.onClickedSix)
+        RBlayout.addWidget(sixhRB, 0, 1)
+
+        hlayout.addLayout(RBlayout)
+
         self.rainButton = QPushButton("Rain")
         hlayout.addWidget(self.rainButton)
         self.tempButton = QPushButton("Temp")
@@ -65,6 +87,7 @@ class MatplotlibWidget(QMainWindow):
         hlayout.addWidget(self.bothButton)
         vlayout.addLayout(hlayout)
         self.x = [dt.datetime.strptime(d,'%d,%m,%Y').date() for d in dates]
+        self.xhours = [dt.datetime.strptime(h,'%H:%M') for h in list(hour_rain.keys())]
 
         rand_temp()
         rand_rain()
@@ -82,97 +105,201 @@ class MatplotlibWidget(QMainWindow):
         self.bothButton.pressed.connect(self.draw_temp_rain)
         self.show()
 
-    def update_plot(self):
-        # new data is plotted
-        self.sc.ax1.cla()
-        data = np.array(np.random.random(3))
-        self.sc.ax1.plot(data)
-        # canvas gets updated by redrawing (if this line is omitted, nothing gets updated
-        self.sc.draw()
-
     def draw_rain(self):
         #Draw barchart of rain
-        y = daily_rain 
-        self.sc.ax1.cla()
-        self.sc.ax2.set_visible(False)
-        self.sc.ax1.bar(self.x,y)
-        self.sc.ax1.grid(True, linestyle='--', color = 'b')
-        self.sc.ax1.set_ylabel('mm', color='b', rotation=0)
-        self.sc.ax1.set_title('Rain')     
-        self.format_xaxis()
-        self.sc.draw()
+        if self.timespan =='month':
+            y = daily_rain 
+            self.sc.ax1.cla()
+            self.sc.ax2.set_visible(False)
+            self.sc.ax1.bar(self.x,y)
+            self.sc.ax1.grid(True, linestyle='--', color = 'b')
+            self.sc.ax1.set_ylabel('mm', color='b', rotation=0)
+            self.sc.ax1.set_title('Rain')     
+            self.format_xaxis()
+            self.sc.draw()
+        else:
+            y = list(hour_rain.values())
+
+            self.sc.ax1.cla()
+            self.sc.ax2.set_visible(False)
+            self.sc.ax1.bar(self.xhours, y, width=0.01)
+            self.sc.ax1.grid(True, linestyle='--', color = 'b')
+            self.sc.ax1.set_ylabel('mm', color='b', rotation=0)
+            self.sc.ax1.set_title('Rain')     
+            self.format_xaxis()
+            self.sc.draw()
+
 
     def draw_temp(self):
         #Draw linegraph of temperature
-        y = daily_temp
-        self.sc.ax1.cla()
-        self.sc.ax2.set_visible(False)
-        self.sc.ax1.plot(self.x, y,'r-')
-        self.sc.ax1.grid(True, linestyle='--', color = 'r')
-        self.sc.ax1.set_ylabel('°C', color='r', rotation=0)
-        #plt.ylabel('Temperature in °C') #Jos molemmat graafit piirretään, niin tää otsikko menee oikealle
-        self.sc.ax1.set_title('Temperature')
-        self.format_xaxis()
-        self.sc.draw()
+        if self.timespan =='month':
+            y = daily_temp
+            self.sc.ax1.cla()
+            self.sc.ax2.set_visible(False)
+            self.sc.ax1.plot(self.x, y,'r-')
+            self.sc.ax1.grid(True, linestyle='--', color = 'r')
+            self.sc.ax1.set_ylabel('°C', color='r', rotation=0)
+            #plt.ylabel('Temperature in °C') #Jos molemmat graafit piirretään, niin tää otsikko menee oikealle
+            self.sc.ax1.set_title('Temperature')
+            self.format_xaxis()
+            self.sc.draw()
+        else:
+            y = list(hour_temp.values())
+
+            self.sc.ax1.cla()
+            self.sc.ax2.set_visible(False)
+            self.sc.ax1.plot(self.xhours, y, 'r-')
+            self.sc.ax1.grid(True, linestyle='--', color = 'r')
+            self.sc.ax1.set_ylabel('°C', color='r', rotation=0)
+            self.sc.ax1.set_title('Temperature')     
+            self.format_xaxis()
+            self.sc.draw()
+           
 
     def draw_temp_rain(self):
         #Draw linegraph for temperature and bar chart for rain amount in same figure
-        y1 = daily_temp
-        y2 = daily_rain
+        if self.timespan =='month':
+            y1 = daily_temp
+            y2 = daily_rain
 
-        self.sc.ax1.cla()
-        self.sc.ax2.cla()
-        self.sc.ax2.set_visible(True)
+            self.sc.ax1.cla()
+            self.sc.ax2.cla()
+            self.sc.ax2.set_visible(True)
 
-        #Accessing the y-axes in self's figure and creating second y-axis thah shares x-axis with the first one
-        ax1 = self.sc.ax1
-        ax2 = self.sc.ax2
-        ax1.set_zorder(1) # brings ax1 to front
-        ax1.patch.set_visible(False)
+            #Accessing the y-axes in self's figure and creating second y-axis thah shares x-axis with the first one
+            ax1 = self.sc.ax1
+            ax2 = self.sc.ax2
+            ax1.set_zorder(1) # brings ax1 to front
+            ax1.patch.set_visible(False)
 
-        # Plot line graph with red line and bar chart with blue bars, add labels to axes
-        ax1.plot(self.x, y1,'r-', label="Temperature")
-        ax2.bar(self.x, y2, label="Rain")
-        ax1.set_ylabel('°C', color='r', rotation=0)
-        ax2.set_ylabel('mm', color='b', rotation=0)
+            # Plot line graph with red line and bar chart with blue bars, add labels to axes
+            ax1.plot(self.x, y1,'r-', label="Temperature")
+            ax2.bar(self.x, y2, label="Rain")
+            ax1.set_ylabel('°C', color='r', rotation=0)
+            ax2.set_ylabel('mm', color='b', rotation=0)
 
-        #Setting secondary y-axis ticks on a wanted 0.5 frequency
-        start, end = ax2.get_ylim()
-        ax2.yaxis.set_ticks(np.arange(start, end, 1.0))
+            #Setting secondary y-axis ticks on a wanted 0.5 frequency
+            start, end = ax2.get_ylim()
+            ax2.yaxis.set_ticks(np.arange(start, end, 1.0))
 
-        # Figure styling + Combined lables box
-        ax1.grid(True, linestyle='--', color = 'r')
-        ax2.grid(True, linestyle='--', color = 'b')
-        #ax1.legend(frameon=True)
-        lines, labels = ax1.get_legend_handles_labels()
-        lines2, labels2 = ax2.get_legend_handles_labels()
-        ax2.legend(lines + lines2, labels + labels2,frameon = True)
+            # Figure styling + Combined lables box
+            ax1.grid(True, linestyle='--', color = 'r')
+            #ax2.grid(True, linestyle='--', color = 'b')
+            #ax1.legend(frameon=True)
+            lines, labels = ax1.get_legend_handles_labels()
+            lines2, labels2 = ax2.get_legend_handles_labels()
+            ax2.legend(lines + lines2, labels + labels2,frameon = True)
 
-        self.format_xaxis()
+            self.format_xaxis()
 
-        self.sc.draw()
+            self.sc.draw()
+        else:
+            y1 = list(hour_temp.values())
+            y2 = list(hour_rain.values())
+
+            self.sc.ax1.cla()
+            self.sc.ax2.cla()
+            self.sc.ax2.set_visible(True)
+
+            #Accessing the y-axes in self's figure and creating second y-axis thah shares x-axis with the first one
+            ax1 = self.sc.ax1
+            ax2 = self.sc.ax2
+            ax1.set_zorder(1) # brings ax1 to front
+            ax1.patch.set_visible(False)
+
+            # Plot line graph with red line and bar chart with blue bars, add labels to axes
+            ax1.plot(self.xhours, y1,'r-', label="Temperature")
+            ax2.bar(self.xhours, y2, width=0.01, label="Rain")
+            ax1.set_ylabel('°C', color='r', rotation=0)
+            ax2.set_ylabel('mm', color='b', rotation=0)
+
+            #Setting secondary y-axis ticks on a wanted 0.5 frequency
+            start, end = ax2.get_ylim()
+            ax2.yaxis.set_ticks(np.arange(start, end, 0.5))
+
+            # Figure styling + Combined lables box
+            ax1.grid(True, linestyle='--', color = 'r')
+            #ax2.grid(True, linestyle='--', color = 'b')
+            #ax1.legend(frameon=True)
+            lines, labels = ax1.get_legend_handles_labels()
+            lines2, labels2 = ax2.get_legend_handles_labels()
+            ax2.legend(lines + lines2, labels + labels2,frameon = True)
+
+            self.format_xaxis()
+            self.sc.draw()
 
     def format_xaxis(self):
-        # X-axis formatting to show dates the wanted way and setting borders to sensible visible are
-        self.sc.ax1.xaxis.set_major_formatter(mdates.DateFormatter("%d-%m-%Y"))
-        self.sc.ax1.xaxis.set_major_locator(mdates.DayLocator())
-        self.sc.ax1.set_xlabel('Date')
-        self.sc.figure.autofmt_xdate()
+        if self.timespan =='month':
+            # X-axis formatting to show dates the wanted way and setting borders to sensible visible are
+            self.sc.ax1.xaxis.set_major_formatter(mdates.DateFormatter("%d-%m-%Y"))
+            self.sc.ax1.xaxis.set_major_locator(mdates.DayLocator())
+            self.sc.ax1.set_xlabel('Date')
+            self.sc.figure.autofmt_xdate()
 
-        #Set x-axis boundaries with one extra day in both ends
-        # other ways to do it: 
-        # #.set_xbound(x[0]-timedelta(days=1), x[len(x)-1]+timedelta(days=1)) 
-        # #.set_xlim([dt.date(2022, 8, 31), dt.date(2022, 10, 1)])
-        self.sc.ax1.set_xbound(self.x[0]-dt.timedelta(days=1), self.x[len(self.x)-1]+dt.timedelta(days=1))
+            #Set x-axis boundaries with one extra day in both ends
+            self.sc.ax1.set_xbound(self.x[0]-dt.timedelta(days=1), self.x[len(self.x)-1]+dt.timedelta(days=1))
+        else:
+            hours = list(hour_rain.keys())
+            x = [dt.datetime.strptime(h,"%H:%M") for h in hours]
+            self.sc.ax1.xaxis.set_major_formatter(mdates.DateFormatter("%H:%M"))
+            #self.sc.ax1.xaxis.set_major_locator(mdates.DayLocator())
+            self.sc.ax1.set_xlabel('Time')
+            self.sc.figure.autofmt_xdate()
+
+            self.sc.ax1.set_xbound(x[0]-dt.timedelta(hours=1), x[len(x)-1]+dt.timedelta(hours=1))
+            
+
+    def onClickedMonth(self):
+        radioButton = self.sender()
+        if radioButton.isChecked():
+            self.timespan = 'month'
+            self.draw_temp_rain()
+        else:
+            pass
+ 
+    def onClickedSix(self):
+        radioButton = self.sender()
+        if radioButton.isChecked():
+            self.timespan = 'sixhours'
+            self.draw_temp_rain()
+        else:
+            pass
 
 app = QApplication([])
 window = MatplotlibWidget()
 app.exec_()
 
 
+"""
+hour_rain = {   
+    '08:00': 1.0, 
+    '09:00': 3.5, 
+    '10:00': 2.0, 
+    '11:00': 4.0, 
+    '12:00': 5.5, 
+    '13:00': 0.0   
+} 
+
+hour_temp = {
+    '08:00':8, 
+    '09:00':10, 
+    '10:00':12, 
+    '11:00':13, 
+    '12:00':17, 
+    '13:00':15
+} 
+
+hours = [hour_rain.keys()]
+y = [hour_rain.values()]
+format = "%H:%M"
+x = [dt.datetime.strptime(h,format).date() for h in hours]
 
 
-
+hours= [hour_temp.keys()]
+y= [hour_temp.values()]
+format = "%H:%M"
+x = [dt.datetime.strptime(h,format).date() for h in hours]
+"""
 
 
 
